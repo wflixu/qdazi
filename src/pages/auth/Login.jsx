8008fb1e4937e293b13c1f2132f9b260d7161b98
wflixu <wflixu@88.com>
2023-12-18 23:37:46 +0800

    fix cors

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -6,20 +6,49 @@ import { EyeInvisibleOutline, EyeOutline } from 'antd-mobile-icons'
 import './login.css'
 import { http, BASE_URL } from '../../shared/http'
 import { useSystemStore } from './../../store/system'
-import { useNavigate } from "react-router-dom";
+import { json, useNavigate } from "react-router-dom";
 
 
 
 
 export function Login() {
     const [visible, setVisible] = useState(false)
-    const setToken =  useSystemStore(state => state.setToken)
-    const setUser =  useSystemStore(state => state.setUser)
+    const setToken = useSystemStore(state => state.setToken)
+    const setUser = useSystemStore(state => state.setUser)
     const navigate = useNavigate();
 
-
-    const onFinish = (values) => {
-        console.warn(values);
+     const fetchdata = async (values) => {
+        try {
+    
+            const response = await fetch('http://121.4.46.56:8443/passport/login', {
+                // fetch('https://www.wflixu.cn/api/passport/login', {
+                method: 'POST',
+                body: JSON.stringify(values),
+                mode: 'cors',
+                redirect: "manual", // manual, *follow, error
+                referrerPolicy: "no-referrer",
+            });
+            const res =  response.json();
+    
+            if (res.code == 200) {
+                const { token, user } = res.data;
+                console.log(token, user)
+                setToken(token)
+                setUser(user);
+                navigate('/home')
+            }
+        } catch (error) {
+            console.log(error)
+        }
+       
+     }
+    const onFinish =  (values) => {
+        // console.warn(values);
+        // fetchdata(values)
+        
+        // // }).catch(error => {
+        // //     console.log(error)
+        // // })
         http.post('/passport/login', values).subscribe(res => {
             console.log(res)
             if (res.code == 200) {
@@ -37,18 +66,18 @@ export function Login() {
     return (
         <div className="login-page">
             <h2 className='h-2 text-center'> 用户登录</h2>
-            <Form onFinish={onFinish} layout='horizontal' mode='card'  footer={
+            <Form onFinish={onFinish} layout='horizontal' mode='card' footer={
                 <Button block type='submit' color='primary' size='large'>
                     提交
                 </Button>
             }>
 
                 <Form.Item label='用户名' name="username" rules={[{ required: true, message: '用户名不能为空' }]}>
-                    <Input placeholder='请输入'   />
+                    <Input placeholder='请输入' />
                 </Form.Item>
                 <Form.Item label='密码' name="password" rules={[{ required: true, message: '用户名不能为空' }]}>
                     <div className="password">
-                        <Input placeholder='请输入' type={visible ? 'text' : 'password'}  />
+                        <Input placeholder='请输入' type={visible ? 'text' : 'password'} />
                         <div className="eye">
                             {!visible ? (
                                 <EyeInvisibleOutline onClick={() => setVisible(true)} />
