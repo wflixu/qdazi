b8a2b18d497ac0eff41fc0aef40ffb6cc0bd2a77
wflixu <wflixu@88.com>
2023-12-22 12:26:36 +0800

    优化图表

--- src/pages/todo/expenditure/GasConsumeLine.jsx
+++ src/pages/todo/expenditure/GasConsumeLine.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Line } from '@ant-design/charts';
+import { Line, DualAxes } from '@ant-design/charts';
 import { http } from '../../../shared/http';
 import { useEffect, useState } from 'react';
 import dayjs from 'dayjs'
@@ -9,12 +9,25 @@ export function GasConsumeLine() {
    useEffect(() => {
       http.get('/expend/?kind=1').subscribe(res => {
          if (res.code == 200) {
-            setData(res.data.map(item => {
-               return {
-                  ...item,
-                  created: new Date(item.created).getTime()
+            const data = [];
+            const aDay = 1000 * 60 * 60 * 24;
+            res.data.forEach((item, index, arr) => {
+               let trend = 0;
+               if (index !== 0) {
+                  let diff = item.count - arr[index - 1].count;
+                  let span = (new Date(item.created).getTime() - new Date(arr[index - 1].created).getTime()) / aDay
+                  trend = diff / span;
                }
-            }))
+               data.push(
+                  {
+                     ...item,
+                     created: new Date(item.created).getTime(),
+                     trend,
+                  }
+               )
+
+            })
+            setData(data)
          }
       })
    }, [])
@@ -23,35 +36,85 @@ export function GasConsumeLine() {
       data,
       height: 400,
       xField: 'created',
-      yField: 'count',
+      children: [
+         {
+            type: 'line',
+            yField: 'trend',
+            style: {
+               lineWidth: 2,
+            },
+            tooltip: {
+               items: [
+                  {
+                     name: '每天平均用量：',
+                     field: 'trend',
+                     valueFormatter: (v) => {
+                        return v.toFixed(2);
+                     }
+                  },
+               ]
+            },
+            axis: {
+               x: {
+                  type: 'time',
+                  arrow: true,
+                  line: true,
+                  labelFormatter: (datum, index, data) => {
+                     return dayjs(datum).format('MM-DD HH:mm')
+                  },
+                  tick: true,
+
+               },
+            },
+         },
+         {
+            type: 'line',
+            yField: 'count',
+            style: {
+               stroke: '#5AD8A6',
+               lineWidth: 2,
+            },
+            tooltip: {
+               items: [
+                  {
+                     name: '当前用量：',
+                     field: 'count',
+                     valueFormatter: (v) => {
+                        return v.toFixed(0);
+                     }
+                  },
+               ]
+      
+            },
+            axis: {
+               x: {
+                  type: 'time',
+                  arrow: true,
+                  line: true,
+                  labelFormatter: (datum, index, data) => {
+                     return dayjs(datum).format('MM-DD HH:mm')
+                  },
+                  tick: true,
+
+               },
+               y: { position: 'right' }
+            },
+         },
+      ],
       label: {
          formatter: function (text, item) {
-            return text
+            return text.toFixed(2)
          }
       },
       tooltip: {
          title: ({ created }) => {
             return dayjs(created).format('MM-DD HH:mm');
-         }
-      },
-
-      style: {
-         lineWidth: 2,
-      },
-      axis: {
-         x: {
-            type: 'time',
-            arrow: true,
-            line: true,
-            labelFormatter: (datum, index, data) => {
-               return dayjs(datum).format('MM-DD HH:mm')
-            },
-            tick: true,
-
          },
-         y: {},
+
       },
-   
+
+
+
    };
-   return <Line {...config} />;
+   return <DualAxes {...config} />;
 }
